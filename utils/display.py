b1e7886a9aff0cd23771f38620fc6f6d0f2304bb
Pierre-Louis Ruhlmann <ruhlmannpl@gmail.com>
2023-05-24 10:20:35 +0100

    last commit maybe

--- utils/display.py
+++ utils/display.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import ticker, pyplot as plt
 
 
-def show_flag(flag: np.ndarray, title = None):
+def show_flag(flag: np.ndarray, title = None, save=False):
     if not title:
         title = 'A sample of the flag dataset'
     plt.imshow(flag)
@@ -10,3 +10,6 @@ def show_flag(flag: np.ndarray, title = None):
     ax = plt.gca()
     ax.xaxis.set_major_locator(ticker.NullLocator())
     _ = ax.yaxis.set_major_locator(ticker.NullLocator())
+    if save:
+        plt.title("")
+        plt.savefig('/home/pl/flags.pdf')
